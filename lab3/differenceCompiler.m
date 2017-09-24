20ca6e5630c75f0ff6bd0dc904e71e603799ecd4
Sam Cook <samuel.cook@temple.edu>
2017-09-24 15:10:32 -0400

    Added graph for pi approx via taylor series, added reverse computing the difference for sums.m, calculate sums.m form 10^1 to 10^7

---
+++ lab3/differenceCompiler.m
@@ -0,0 +1,11 @@
+function differenceCompiler()
+
+[s1, d1] = sums(10)
+[s2, d2] = sums(100)
+[s3, d3] = sums(1000)
+[s4, d4] = sums(10000)
+[s5, d5] = sums(100000)
+[s6, d6] = sums(1000000)
+[s7, d7] = sums(10000000)
+
+end
--- lab3/sums.m
+++ lab3/sums.m
@@ -1,7 +1,8 @@
 %N = 10^1 through 10^7
 
-function [aSing, aDub, bSing, bDub, cSing, cDub, dSing, dDub] = sums (n)
+function [singleDifferences, doubleDifferences] = sums (n)
 
+%set to zero, both single and double precision
 aSing = single(0);
 aDub = 0;
 bSing = single(0);
@@ -11,9 +12,10 @@ cDub = 0;
 dSing = single(0);
 dDub = 0;
 
+%smallest to largest
 for i=n:-1:1
         
-    aSing = single(aSing + (1/i))
+    aSing = single(aSing + (1/i));
     aDub = aDub + (1/i);
     
     bSing = single(bSing + (1/i^2));
@@ -22,16 +24,52 @@ for i=n:-1:1
     cSing = single(cSing + (1/i^3));
     cDub = cDub + (1/i^3);
     
-%     dSing = single(dSing + (((-1)^i)/i));
-%     dDub = dDub + (((-1)^i)/i);
-    
+    dSing = single(dSing + (((-1)^i)/i));
+    dDub = dDub + (((-1)^i)/i);
+   
 end
 
-aSing
-aDub
-bSing 
-bDub 
-cSing 
-cDub
-dSing
-dDub
+aSingRev = single(0);
+aDubRev = 0;
+bSingRev = single(0);
+bDubRev = 0;
+cSingRev = single(0);
+cDubRev = 0;
+dSingRev = single(0);
+dDubRev = 0;
+
+%largest to smallest
+for i=1:n
+        
+    aSingRev = single(aSingRev + (1/i));
+    aDubRev = aDubRev + (1/i);
+    
+    bSingRev = single(bSingRev + (1/i^2));
+    bDubRev = bDubRev + (1/i^2);
+    
+    cSingRev = single(cSingRev + (1/i^3));
+    cDubRev = cDubRev + (1/i^3);
+    
+    dSingRev = single(dSingRev + (((-1)^i)/i));
+    dDubRev = dDubRev + (((-1)^i)/i);
+   
+end
+
+aSingDiff = abs(aSingRev - aSing);
+aDubDiff = abs(aDubRev - aDub);
+
+bSingDiff = abs(bSingRev - bSing);
+bDubDiff = abs(bDubRev - bDub);
+
+cSingDiff = abs(cSingRev - cSing);
+cDubDiff = abs(cDubRev - cDub);
+
+dSingDiff = abs(dSingRev - dSing);
+dDubDiff = abs(dDubRev - dDub);
+
+
+singleDifferences = [aSingDiff, bSingDiff, cSingDiff, dSingDiff];
+doubleDifferences = [aDubDiff, bDubDiff, cDubDiff, dDubDiff];
+
+
+
