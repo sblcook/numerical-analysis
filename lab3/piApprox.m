f54996cd2ebfb97144abf839805390af50f0deee
Sam Cook <samuel.cook@temple.edu>
2017-09-23 21:22:48 -0400

    fixed pi approximation, now very accurately approximates pi. Still need to plot results

--- lab3/piApprox.m
+++ lab3/piApprox.m
@@ -1,16 +1,19 @@
 function [tanDiff, sinDiff] = piApprox(termsInSum)
 
+format long;
+
 tanSeries = 0; %arctan(x) = series
-sinSeries = 0;
+sinSeries = 0; %arcsin(x) = series
 
 n = termsInSum;
+xForSin = 0.5;
 
 for i=0:n
-    
-    tanSeries = tanSeries + ( ((-1)^n) / (2*n + 1) ); %x = 1
-    sinSeries = sinSeries + ( (1/(2^(2*n))) * nchoosek(2*n, n) / ((2*n) + 1) );
+
+    tanSeries = tanSeries + ( ((-1)^i) / ((2*i) + 1) ); %x = 1
+    sinSeries = sinSeries + ( (1/(2^(2*i))) * nchoosek(2*i, i) * (xForSin^(2*i + 1)) / ((2*i) + 1) );
     
 end
 
-tanSeries
-sinSeries
+sinDiff = abs((6 * sinSeries) - pi);
+tanDiff = abs((4 * tanSeries) - pi);
