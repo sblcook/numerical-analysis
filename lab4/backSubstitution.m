788a8cd311adf15bddb6ae054040e4db9409a52e
Sam Cook <samuel.cook@temple.edu>
2017-09-30 17:47:47 -0400

    partial pivoting now works, modifies "b" solution vector while doing gauss elimination

--- lab4/backSubstitution.m
+++ lab4/backSubstitution.m
@@ -3,14 +3,10 @@
 function [x] = backSubstitution(U, b);
 %U an n by n matrix, b an n by 1 vector, x the n by 1 solution vector
 
-[n, n] = size(U);
+[m, n] = size(U);
 x = zeros(n, 1);
-x(n) = b(n) / U(n, n);
 
-for i=n-1:-1:1
-    sum = b(i);
-    for j=i+1:n
-        sum = sum - U(i, j) * x(j);
-    end
-    x(i) = sum/U(i,i);
+for j=n:-1:1
+    x(j) = b(j) / U(j, j);
+    b(1:j-1) = b(1:j-1) - U(1:j-1, j) * x(j);
 end
--- lab4/gaussElimPartialPivoting.m
+++ lab4/gaussElimPartialPivoting.m
@@ -1,9 +1,10 @@
-function U=gaussElimPartialPivoting(M)
+function [U, swappedAnswerVector] = gaussElimPartialPivoting(M, answerVector)
 % gaussian elimination with full pivoting
 [m,n] = size(M);
 k=1;
 j=1;
-
+M = [M answerVector];
+M
 while (j <= m) && (k <= n)
    
     a=abs(M(j:end,k)); %find absolute value below the pivot position
@@ -13,20 +14,20 @@ while (j <= m) && (k <= n)
         continue    % if all zeros below (and possibly including) the pivot, no need to swap rows, so move on to next column, 
                     % but stay on same row! <- this is where k is important
         
-    elseif (max(a) > 0) && (M(j,k) == 0)  %need to deal with case that entry in the pivot position is zero
+    elseif (max(abs(a)) > 0)  
         b=M(j:end,k); %set b to be portion of k-th column below (and including) (j,k) position
-        [~,indices] = max(b); %finds max value in vector b, adds it to indices vector
+        [~,indices] = max(abs(b)); %finds max value in vector b, adds it to indices vector
         idx = indices(1); %selects first index of max value
-        
-        
         idx=idx+j-1; %reindex to get correct index for full j-th column
         
         %swap rows to get rid of zero in pivot position
         swap = M(j,:);
         M(j,:) = M(idx,:); 
         M(idx,:) = swap;
+        
     end
     
+    M
     for i = j+1:m
         M(i,:) = M(i,:) - (M(i,k)/M(j,k))*M(j,:); % perform type III operations 
         M %print M to show intermediate steps
@@ -35,4 +36,5 @@ while (j <= m) && (k <= n)
     k=k+1;
 end
 
-U=M;
+U=M(1:m, 1:n);
+swappedAnswerVector = M(:,n+1);
