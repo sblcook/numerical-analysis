44543b96bf88e6f80c49552af9c137dc0e96f28a
Sam Cook <samuel.cook@temple.edu>
2017-11-06 12:15:41 -0500

    Programmed Newton's Method for finding roots

---
+++ rootfinding/newtons_method.m
@@ -0,0 +1,14 @@
+function [x, req_iter] = newtons_method(f, fprime, e, x0, max_iter, a, b)
+%This function uses Newton's Method to find the roots of function f
+%Input: f, f', error, initial guess, max number of iterations, range
+%Output: 
+
+req_iter = 0; %how many iterations have been required
+x = x0; %our approximation
+
+while (abs(f(x)) > e) && (req_iter < max_iter)
+    x = x - (f(x)/fprime(x));
+    req_iter = req_iter + 1;
+end
+
+end
