0127808b9ab473bc61ae4a598eb683a64929c112
Sam Cook <samuel.cook@temple.edu>
2017-11-06 11:11:46 -0500

    Coded gradient method

--- gradient/gradient.m
+++ gradient/gradient.m
@@ -1,18 +1,22 @@
 function [x, N] = gradient(A, b, x0, e)
+%use the gradient method to solve Ax=b given an initial guess x0
+%and en error bound e
 
+%return the approximate solution, x, and the number of iterations it took, N
+
+%calculate initial values
 r = b-A*x0;
 d = r;
 N = 0;
 x = x0;
-gradient = 2*A'*(Ax-b)
 
-while (r<e):
-    
-   l = (d' * r)/(d'*A*d);
-   x = x + l*d;
-   r = b - A*x;
-   d = r;
-   N = N+1;
+while (norm(r, 2) > e)    
+   l = (d' * r)/(d'*A*d); %optimal step size
+   x = x + l*d;  %new solution
+   r = b - A*x; %residual
+   d = r; %set direction equal to residual for gradient
+   N = N+1; %increment number of iterations
 end
 
 end
+
