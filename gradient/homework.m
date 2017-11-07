a51e9187da918861e938381e025e79479a585f09
Sam Cook <samuel.cook@temple.edu>
2017-11-07 12:49:46 -0500

    In homework script, calculating the error for each iteration for both matrices and both methods

--- gradient/homework.m
+++ gradient/homework.m
@@ -12,7 +12,13 @@ x_0 = ones([m, 1]);
 x1_0 = ones([m, 1]);
 
 [x, N] = conjugate_gradient(A, -b, x_0, 10e-8);
+[y, M] = gradient(A, -b, x_0, 10e-8);
 [x1, N1] = conjugate_gradient(A1, -b1, x1_0, 10e-8);
+[y1, M1] = gradient(A1, -b1, x1_0, 10e-8);
 
+[cg_error] = conjugate_gradient_iterations(A, -b, x_0, max(N, N1), x);
+[g_error] = gradient_iterations(A, -b, x_0, max(M, M1), y);
+[cg1_error] = conjugate_gradient_iterations(A1, -b1, x1_0, max(N,N1), x1);
+[g1_error] = gradient_iterations(A1, -b1, x1_0, max(M, M1), y1);
 
 
