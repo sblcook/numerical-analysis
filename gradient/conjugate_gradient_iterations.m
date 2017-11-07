96671eb41c0a994bb3d9313fa453c8a9c636ccb8
Sam Cook <samuel.cook@temple.edu>
2017-11-06 22:16:10 -0500

    conjugate_gradient_iterations returns error from each iteration

--- gradient/conjugate_gradient_iterations.m
+++ gradient/conjugate_gradient_iterations.m
@@ -1,15 +1,16 @@
-function [x] = conjugate_gradient_iterations(A, b, x0, N)
+function [error] = conjugate_gradient_iterations(A, b, x0, N, exact_x)
 %use the conjugate gradient method to solve Ax=b given an initial guess x0
 %and en error bound e
 
-%return the approximate solution, x, and the number of iterations it took, N
+%return the error at each iteration
 
 %calculate initial values
 x = x0;
 r_old = b-A*x0;
 d = r_old;
+error = zeros(N,1);
 
-for i = 0:N
+for i = 1:N
     %we keep track of the new residual and the old residual because it is
     %required for caluclating B
     l = (r_old'*r_old)/(d'*A*d); %optimal step size
@@ -18,6 +19,7 @@ for i = 0:N
     B = (r_new'*r_new)/(r_old'*r_old); %new B
     d = r_new + B*d; %new direction
     r_old = r_new; %update the residual
+    error(i) = norm(x - exact_x); %update the error
 end
 
 end
