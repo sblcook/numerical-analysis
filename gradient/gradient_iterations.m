0d5ce21b9ae91676a7cc487d5e2720a952f1ba40
Sam Cook <samuel.cook@temple.edu>
2017-11-06 22:11:41 -0500

    Modified gradient_iterations to return error at each iteration

--- gradient/gradient_iterations.m
+++ gradient/gradient_iterations.m
@@ -1,19 +1,21 @@
-function [x] = gradient_iterations(A, b, x0, N)
+function [error] = gradient_iterations(A, b, x0, N, exact_x)
 %use the gradient method to solve Ax=b given an initial guess, x0,
 %and how many iterations to complete, N
 
-%return the approximate solution, x
+%return the error for each iteration
 
 %calculate initial values
 r = b-A*x0;
 d = r;
 x = x0;
+error = zeros(N, 1);
 
-for i=0:N   
+for i=1:N   
    l = (d' * r)/(d'*A*d); %optimal step size
    x = x + l*d;  %new solution
    r = b - A*x; %residual
    d = r; %set direction equal to residual for gradient
+   error(i) = norm(exact_x - x); %get error
 end
 
 end
